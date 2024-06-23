acf100cb659b4c6e469c9743c7c7c2ae91f161a8
Rohn Allen Santiago <rsantiag@tcd.ie>
2024-06-23 19:44:13 +0100

    Added function to calculate time taken by one bounce

--- rhythm.py
+++ rhythm.py
@@ -1,6 +1,15 @@
+from math import *
+
 # Constants
 MAX_HEIGHT = 600
 HEIGHT_OFFSET = 100
 
+# Calculating path of bouncing circle
+# Since hte height a circle travels will be the same regardless of tempo or rhythm (fixed height),
+# we are only looking for time for one bounce (t), initial velocity (u) and acceleration (a)
 
-
+# Calculate t given the rhythm (number of bounces per beat) and tempo (number of beats per minute)
+def calc_t(rhythm, tempo):
+    beats = rhythm * tempo
+    bps = beats / 69
+    return 1 / bps
