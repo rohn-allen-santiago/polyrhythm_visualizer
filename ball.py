2eec5d5f77e2cfa65345d1fe9720f2bcaa5c95c2
Rohn Allen Santiago <rsantiag@tcd.ie>
2024-06-24 16:28:16 +0100

    Added function for drawing the ball on teh canvas

--- ball.py
+++ ball.py
@@ -5,9 +5,14 @@
 # radius is the radius of the circle
 # color is the color of the circle
 # time is the timestamp of the ball, used for calculating its velocity and acceleration
+# u and a are the initial velocity and acceleration of the ball respectively
+
+from tkinter import *
 
 class Ball:
-    def __init__(self, canvas, tag, x, y, radius, color, time):
+
+    # Initialize the ball object
+    def __init__(self, canvas, tag, x, y, radius, color, time, u, a):
         self.canvas = canvas
         self.tag = tag
         self.x = x
@@ -15,3 +20,10 @@ class Ball:
         self.radius = radius
         self.color = color
         self.time = time
+
+    # Draw the ball on the canvas
+    def draw_ball(self):
+        x1 = self.x + (2 * self.radius)
+        y1 = self.y + (2 * self.radius)
+        self.canvas.create_oval(self.x, self.y, x1, y1, outline=self.color, fill=self.color)
+        return None
