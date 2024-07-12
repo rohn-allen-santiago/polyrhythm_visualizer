83d0724508977b40141d5429f42cb15fdad3af02
Rohn Allen Santiago <rsantiag@tcd.ie>
2024-07-12 16:35:50 +0100

    Added function to change rhythm of each ball

--- main.py
+++ main.py
@@ -15,12 +15,13 @@ tempoLabel = Label(f, text="Tempo")
 tempoLabel.grid(row=0,column=0, pady=5)
 tempoInput = Entry(f, bg="white")
 tempoInput.grid(row=0,column=1, pady=5)
-ballInputs = []
+rhythms = []
 for i in range(5):
     name = "Ball " + str(i + 1)
     Label(f, bg="white", text=name).grid(row=i+1, column=0, pady=5)
-    entry = Entry(f, bg="white").grid(row=i+1, column=1, pady=5)
-    ballInputs.append(entry)
+    entry = Entry(f, bg="white")
+    entry.grid(row=i+1, column=1, pady=5)
+    rhythms.append(entry)
 
 # Create canvas and set dimensions
 canvas = Canvas(pv, width=1500, height=800, bg="white")
@@ -32,31 +33,43 @@ ceiling = canvas.create_line(0, 100,1500, 100)
 
 # Add ball(s) to canvas (testing)
 balls = []
-ball1 = Ball(canvas, "ball1", 400, "blue")
-ball1.set_rhythm(2)
-ball1.set_speed()
+ball1 = Ball(canvas, "ball1", 250, "blue")
 ball1.draw()
 balls.append(ball1)
-ball2 = Ball(canvas, "ball2", 800, "red")
-ball2.set_rhythm(3)
-ball2.set_speed()
+ball2 = Ball(canvas, "ball2", 500, "red")
 ball2.draw()
 balls.append(ball2)
-ball3 = Ball(canvas, "ball3", 1200, "green")
-ball3.set_rhythm(4)
-ball3.set_speed()
+ball3 = Ball(canvas, "ball3", 750, "green")
 ball3.draw()
 balls.append(ball3)
+ball4 = Ball(canvas, "ball4", 1000, "yellow")
+ball4.draw()
+balls.append(ball4)
+ball5 = Ball(canvas, "ball5", 1250, "purple")
+ball5.draw()
+balls.append(ball5)
 
-# Set tempo for each ball to global tempo
+# Set tempo for each ball to user inputted tempo
 def change_tempo():
     tempo = tempoInput.get()
     if tempo == "":
-        tempo = "60"
+        return None
     tempo = int(tempo)
     for ball in balls:
         ball.set_tempo(tempo)
         ball.set_speed()
+    return None
+
+# Set rhythm for each ball to user inputted rhythm
+def change_rhythm():
+    for i in range(5):
+        print(type(rhythms))
+        rhythm = rhythms[i].get()
+        if rhythm == "":
+            continue
+        rhythm = int(rhythm)
+        balls[i].set_rhythm(rhythm)
+        balls[i].set_speed()
     return None
 
 # Run main loop
@@ -64,5 +77,6 @@ while True:
     pv.update_idletasks()
     pv.update()
     change_tempo()
+    change_rhythm()
     for ball in balls:
         ball.update()
